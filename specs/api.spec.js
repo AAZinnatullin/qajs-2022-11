b73b662c3848167dbe29f4d029840217cf3f208c
Artyom <artyom.zn@plyo.io>
2022-12-23 00:10:12 +0400

    [Homework5] Added tests for API Bookstore

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,39 +1,119 @@
 import axios from "axios";
-import {expect, test} from "@jest/globals";
+import { expect, it } from "@jest/globals";
+import { faker } from '@faker-js/faker';
 
-test('GET request', async () => {
-    const config = {
-        method: "get",
-        url: "https://dummyjson.com/products/1",
-    };
-    const resp = await axios(config);
-    expect(resp.data.title).toBe('iPhone 9');
-});
+const userName = faker.name.fullName();
 
-test('POST request', async () => {
+it('Checks user create successfully', async () => {
     const config = {
-        method: "post",
-        url: "https://dummyjson.com/products/add",
-        headers: "application/json",
+        method: "POST",
+        url: "https://bookstore.demoqa.com/Account/v1/User",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
         data: {
-            "title": "new Title",
-            "price": 123
+            "userName": `${userName}`,
+            "password": "Password21!"
         },
     };
-    const resp = await axios(config);
-    expect(resp.data.title).toBe('new Title');
-    expect(resp.data.price).toBe(123);
-    expect(resp.status).toBe(200)
+    try {
+        const res = await axios(config);
+        expect(res.status).toBe(201);
+    } catch (e) {
+        console.log(e);
+    }
 });
 
-test('Check products with auth', async () => {
+it('Checks error for create existing users', async () => {
     const config = {
-        method: "get",
-        url: "https://dummyjson.com/auth/products/",
+        method: "POST",
+        url: "https://bookstore.demoqa.com/Account/v1/User",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        data: {
+            "userName": `${userName}`,
+            "password": "Password21!"
+        },
     };
     try {
-        const resp = await axios(config);
-        expect(resp.response.status).toBe(403);
+        const res = await axios(config);
+        expect(res.status).toBe(406);
+    } catch (e) {
+        expect(e.response.data.message).toBe('User exists!');
+    }
+});
+
+it.each([
+    'password12!',
+    'Password121',
+    'Password!',
+    'Pass!21',
+])('Checks error when user use sipmle password', async (password) => {
+    const config = {
+        method: "POST",
+        url: "https://bookstore.demoqa.com/Account/v1/User",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        data: {
+            "userName": 'New username',
+            "password": `${password}`
+        },
+    };
+    try {
+        const res = await axios(config);
+        expect(res.status).toBe(400);
+    } catch (e) {
+        expect(e.response.data.message).toBe(
+            'Passwords must have at least one non alphanumeric character, one digit (\'0\'-\'9\'), ' +
+            'one uppercase (\'A\'-\'Z\'), one lowercase (\'a\'-\'z\'), one special character and Password must ' +
+            'be eight characters or longer.');
+    }
+});
+
+it('Checks error for generation token', async () => {
+    const config = {
+        method: "POST",
+        url: "https://bookstore.demoqa.com/Account/v1/GenerateToken",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        data: {
+            "userName": '',
+            "password": 'password'
+        },
+    };
+    try {
+        const res = await axios(config);
+        expect(res.status).toBe(400);
+    } catch (e) {
+        expect(e.response.data.message).toBe('UserName and Password required.');
+    }
+});
+
+it('Checks successfull generated token', async () => {
+    const config = {
+        method: "POST",
+        url: "https://bookstore.demoqa.com/Account/v1/GenerateToken",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        data: {
+            "userName": `${userName}`,
+            "password": "Password21!"
+        },
+    };
+    try {
+        const res = await axios(config);
+        expect(res.status).toBe(200);
+        expect(res.data.token).not.toBeNull();
+        expect(res.data.result).toBe('User authorized successfully.')
     } catch (e) {
         console.log(e);
     }
